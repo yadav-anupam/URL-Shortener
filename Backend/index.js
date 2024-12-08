09b6553ea7f0a87cd51138a87703e3b1631d8bf6
Anupam Yadav <anupamyadav667@gmail.com>
2024-12-08 21:53:15 +0530

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import urlRouter from "./routes/url.js";
 import userRouter from "./routes/user.js"
-import connectToMongoDb from "./connect.js";
+import connectToMongoDb from "./db/connect.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { configDotenv } from "dotenv";
@@ -9,9 +9,14 @@ configDotenv();
 
 await connectToMongoDb(process.env.MONGOURL);
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.FRONTENDURL,
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
